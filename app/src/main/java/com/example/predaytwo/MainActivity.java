39bbff8097e8d6b18f44c477be4bc092c79b3786
NehaThipse <nehathipse@gmail.com>
2019-06-11 10:03:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/predaytwo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.predaytwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button , button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button1= findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , DisplayImage.class);
+                startActivity(intent);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , ImageSlider.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
